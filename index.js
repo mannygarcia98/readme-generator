b2772bb6ec8814e7e4d02431f9c1259526c7e600
mannygarcia98 <mannysg88@gmail.com>
2022-04-02 16:28:22 -0500

    add inquirer package

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const fs = require("fs");
-const generateReadMe = require("./src/README-template.js");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [title, description] = profileDataArgs;
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generateReadMe = require("./src/README-template.js");
+// // const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const markdown = generateReadMe(title, description);
+// // const [title, description] = profileDataArgs;
 
-fs.writeFile("README_1.md", generateReadMe(title, description), (err) => {
-  if (err) throw err;
+// fs.writeFile("README_1.md", markdown, (err) => {
+//   if (err) throw err;
 
-  console.log("README complete! Check out README_1.html to see the output.");
-});
+//   console.log("README complete! Check out README_1.html to see the output.");
+// });
